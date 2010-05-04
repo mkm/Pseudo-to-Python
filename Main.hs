6d84e9b796936a7613a5b891f595a33fad94d15b
mkm <kragh@diku.dk>
2010-05-04 22:27:18 +0200

    First working compiler.

--- Main.hs
+++ Main.hs
@@ -3,23 +3,15 @@ module Main where
 import Prelude hiding (either)
 import System
 import Control.Monad
+import Data.List
 
 main = getArgs >>= mapM_ (\fileName -> readFile fileName >>= writeFile (fileName ++ ".py") . compile)
 
-compile = runParser sourceFile . unlines . cstyler [0] . lines
+compile = unlines . map compileLine . lines
 
-cstyler _ [] = []
-cstyler indents@(curIndent:indentsRest) (x:xs) = case removeIndent x of
-    (indentLevel, str) -> case compare indentLevel curIndent of
-        LT -> if indentLevel == head indentsRest
-            then "}" : (str ++ ";") : cstyler indentsRest xs
-            else error "wrong indentation"
-        EQ -> (str ++ ";") : cstyler indents xs
-        GT -> "{" : (str ++ ";") : cstyler (indentLevel : indents) xs
-
-removeIndent (' ':xs) = case removeIndent xs of
-    (indentLevel, xs') -> (indentLevel + 1, xs')
-removeIndent xs = (0, xs)
+compileLine input = case parse sourceLine input of
+    Left err -> err
+    Right (indent, str) -> indent ++ stmtToSource str
 
 data Error
     = UnknownError
@@ -37,6 +29,10 @@ instance Monad Parser where
 
 failure = Parser $ \_ -> Left UnknownError
 
+parse p input = case runParser p input of
+    Left err -> Left (show err)
+    Right (x, _) -> Right x
+
 infixl 3 <|>
 p <|> q = Parser $ \str -> case runParser p str of
     Left err -> case runParser q str of
@@ -48,13 +44,22 @@ satisfies pred = Parser $ \str -> case str of
     [] -> Left UnknownError
     (x:xs) -> if pred x then Right (x, xs) else Left UnknownError
 
+notFollowedBy p = Parser $ \str -> case runParser p str of
+    Left err -> Right ((), str)
+    Right _ -> Left UnknownError
+
+anything = satisfies (const True)
+
 zeroOrMore p = oneOrMore p <|> return []
 
 oneOrMore p = return (:) `ap` p `ap` zeroOrMore p
 
 oneOf xs = satisfies (`elem` xs)
 
-seperatedBy p q = p >> zeroOrMore (q >> p)
+seperatedBy p q = do
+    head <- p
+    tail <- zeroOrMore (q >> p)
+    return $ head : tail
 
 either = foldr (<|>) failure
 
@@ -80,12 +85,15 @@ ws = zeroOrMore $ white
 
 wss = oneOrMore $ white
 
+endOfSource = notFollowedBy anything
+
 data Statement
-    = ForLoop Expression Expression Expression [Statement]
-    | WhileLoop Expression [Statement]
-    | FunctionDecl String [String] [Statement]
+    = ForLoop Expression Expression Expression
+    | WhileLoop Expression
+    | FunctionDecl String [String]
     | Assignment Expression Expression
     | ReturnStmt Expression
+    | EmptyStmt
     deriving Show
 
 data Expression
@@ -97,20 +105,25 @@ data Expression
     | Divide Expression Expression
     deriving Show
 
-sourceFile = zeroOrMore functionDecl
+stmtToSource (ForLoop a b c) = "for " ++ exprToSource a ++ " in range(" ++ exprToSource b ++ ", (" ++ exprToSource c ++ ") - 1):"
+stmtToSource (WhileLoop a) = "while " ++ exprToSource a ++ ":"
+stmtToSource (FunctionDecl name params) = "def " ++ name ++ "(" ++ foldr1 (\x y -> x ++ ", " ++ y) params ++ "):"
+stmtToSource (Assignment to from) = exprToSource to ++ " = " ++ exprToSource from
+stmtToSource (ReturnStmt a) = "return " ++ exprToSource a
+stmtToSource EmptyStmt = ""
+
+exprToSource (Variable name) = name
+exprToSource (Constant value) = value
+
+sourceLine = do
+    indent <- ws
+    x <- statement <|> return EmptyStmt
+    endOfSource
+    return (indent, x)
 
 statement = either [forLoop, whileLoop, functionDecl, assignment, returnStmt]
 
 expression = either [variable, constant{-, plus, minus, times, divide-}]
-
-block = do
-    ws
-    match "{"
-    ws
-    body <- zeroOrMore statement
-    ws
-    match "}"
-    return body
 
 forLoop = do
     ws
@@ -125,20 +138,14 @@ forLoop = do
     match "to"
     ws
     termExpr <- expression
-    ws
-    match ";"
-    body <- block
-    return $ ForLoop loopVar initExpr termExpr body
+    return $ ForLoop loopVar initExpr termExpr
 
 whileLoop = do
     ws
     match "while"
     ws
     termExpr <- expression
-    ws
-    match ";"
-    body <- block
-    return $ WhileLoop termExpr body
+    return $ WhileLoop termExpr
 
 functionDecl = do
     ws
@@ -146,10 +153,7 @@ functionDecl = do
     match "("
     params <- (word `seperatedBy` match ", ") <|> return []
     match ")"
-    ws
-    match ";"
-    body <- block
-    return $ FunctionDecl name params body
+    return $ FunctionDecl name params
 
 assignment = do
     ws
